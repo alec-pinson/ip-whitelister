ed16fa4c517b4a2e7ac524864c40f2282052d4a7
Alec <alec.pinson@fedex.com>
2021-12-28 10:06:38 +0000

    feat: add more redis tests

--- redis_test.go
+++ redis_test.go
@@ -19,7 +19,7 @@ type TestRedisInstance struct {
 	Token    string
 }
 
-func testRedis(t *testing.T) TestRedisInstance {
+func CreateTestRedis(t *testing.T) TestRedisInstance {
 	pool, err := dockertest.NewPool("")
 	if err != nil {
 		t.Errorf("redis_test.TestConnect(): Could not connect to docker: %s", err)
@@ -64,14 +64,14 @@ func testRedis(t *testing.T) TestRedisInstance {
 	return testRedisInstance
 }
 
-func testRedisCleanup(t *testing.T, tr TestRedisInstance) {
+func DeleteTestRedis(t *testing.T, tr TestRedisInstance) {
 	if err := tr.Pool.Purge(&tr.Resource); err != nil {
-		t.Errorf("redis_test.testRedisCleanup(): Could not purge resource: %s", err)
+		t.Errorf("redis_test.DeleteTestRedis(): Could not purge resource: %s", err)
 	}
 }
 
 func TestConnect(t *testing.T) {
-	var testRedisInstance = testRedis(t)
+	var testRedisInstance = CreateTestRedis(t)
 	var rc RedisConfiguration
 	rc.Host = testRedisInstance.Host
 	rc.Port = testRedisInstance.Port
@@ -80,55 +80,136 @@ func TestConnect(t *testing.T) {
 	ret := r.connect(rc)
 
 	if ret != 0 {
-		t.Error()
+		t.Error("redis.connect(): failed to connect, got 'failed connection', want 'successful connection'")
 	}
 
-	testRedisCleanup(t, testRedisInstance)
+	// wrong hostname
+	rc.Host = "nonexiststenthostname"
+	rc.Port = testRedisInstance.Port
+	rc.Token = testRedisInstance.Token
+
+	ret = r.connect(rc)
+	if ret != 1 {
+		t.Errorf("redis.connect(): connect for '%v:%v', got 'success', want 'fail'", rc.Host, rc.Port)
+	}
+
+	// wrong port
+	rc.Host = testRedisInstance.Host
+	rc.Port = 12345
+	rc.Token = testRedisInstance.Token
+
+	ret = r.connect(rc)
+	if ret != 1 {
+		t.Errorf("redis.connect(): connect for '%v:%v', got 'success', want 'fail'", rc.Host, rc.Port)
+	}
+
+	// wrong password
+	rc.Host = testRedisInstance.Host
+	rc.Port = testRedisInstance.Port
+	rc.Token = "wrongpassword!"
+
+	ret = r.connect(rc)
+	if ret != 1 {
+		t.Errorf("redis.connect(): connect for '%v:%v' pw: %v, got 'success', want 'fail'", rc.Host, rc.Port, rc.Token)
+	}
+
+	DeleteTestRedis(t, testRedisInstance)
 }
 
 func TestAddIp(t *testing.T) {
-	var testRedisInstance = testRedis(t)
+	users := []struct {
+		user    string
+		cidr    string
+		success int
+	}{
+		{"testuser111111", "10.0.0.1/32", 0},
+		{"testuser111112", "10.0.0.2/32", 0},
+		{"testuser111113", "10.0.0.3/32", 0},
+		{"testuser111114", "10.0.0.4/32", 0},
+		{"testuser111115", "10.0.0.5/32", 0},
+	}
+
+	var testRedisInstance = CreateTestRedis(t)
 	var rc RedisConfiguration
 	rc.Host = testRedisInstance.Host
 	rc.Port = testRedisInstance.Port
 	rc.Token = testRedisInstance.Token
-	r.connect(rc)
-	ret := r.addIp("testuser11111", "10.0.0.1/32")
-	if ret != 0 {
-		t.Errorf("redis_test.TestAddIp(): Failed to add IP")
+	ret := r.connect(rc)
+	if ret == 0 {
+		for _, f := range users {
+			ret = r.addIp(f.user, f.cidr)
+			if ret != f.success {
+				t.Errorf("redis.addIp(): Add user ip %v, got '%v', want '%v'", f, ret, f.success)
+			}
+		}
 	}
 
-	testRedisCleanup(t, testRedisInstance)
+	DeleteTestRedis(t, testRedisInstance)
 }
 
 func TestDeleteIp(t *testing.T) {
-	var testRedisInstance = testRedis(t)
+	users := []struct {
+		user    string
+		cidr    string
+		success int
+	}{
+		{"testuser111111", "10.0.0.1/32", 0},
+		{"testuser111112", "10.0.0.2/32", 0},
+		{"testuser111113", "10.0.0.3/32", 0},
+		{"testuser111114", "10.0.0.4/32", 0},
+		{"testuser111115", "10.0.0.5/32", 0},
+	}
+
+	var testRedisInstance = CreateTestRedis(t)
 	var rc RedisConfiguration
 	rc.Host = testRedisInstance.Host
 	rc.Port = testRedisInstance.Port
 	rc.Token = testRedisInstance.Token
-	r.connect(rc)
-	r.addIp("testuser11111", "10.0.0.1/32")
-	ret := r.deleteIp("testuser11111")
-	if ret != 0 {
-		t.Errorf("redis_test.TestDeleteIp(): Failed to delete IP")
+	ret := r.connect(rc)
+	if ret == 0 {
+		for _, f := range users {
+			ret = r.addIp(f.user, f.cidr)
+			if ret == f.success {
+				ret = r.deleteIp(f.user)
+				if ret != 0 {
+					t.Errorf("redis.deleteIp(): Delete user ip %v, want '%v', got '%v'", f, ret, f.success)
+				}
+			}
+		}
 	}
 
-	testRedisCleanup(t, testRedisInstance)
+	DeleteTestRedis(t, testRedisInstance)
 }
 
 func TestGetWhitelist(t *testing.T) {
-	var testRedisInstance = testRedis(t)
+	users := []struct {
+		user string
+		cidr string
+	}{
+		{"testuser111111", "10.0.0.1/32"},
+		{"testuser111112", "10.0.0.2/32"},
+		{"testuser111113", "10.0.0.3/32"},
+		{"testuser111114", "10.0.0.4/32"},
+		{"testuser111115", "10.0.0.5/32"},
+	}
+
+	var testRedisInstance = CreateTestRedis(t)
 	var rc RedisConfiguration
 	rc.Host = testRedisInstance.Host
 	rc.Port = testRedisInstance.Port
 	rc.Token = testRedisInstance.Token
-	r.connect(rc)
-	r.addIp("testuser11111", "10.0.0.1/32")
-	ret := r.getWhitelist()
-	if ret["testuser11111"] != "10.0.0.1/32" {
-		t.Errorf("redis_test.TestGetWhitelist(): Failed to get whitelist")
+	ret := r.connect(rc)
+	if ret == 0 {
+		for _, f := range users {
+			ret = r.addIp(f.user, f.cidr)
+			if ret == 0 {
+				ret := r.getWhitelist()
+				if ret[f.user] != f.cidr {
+					t.Errorf("redis.getWhitelist(): Get whitelist %v, got '%v', want '%v'", f, f.cidr, ret[f.user])
+				}
+			}
+		}
 	}
 
-	testRedisCleanup(t, testRedisInstance)
+	DeleteTestRedis(t, testRedisInstance)
 }
